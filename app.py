b1ef06568d8654bf1ae42021f7a5d58ee27ae6eb
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-04-08 14:29:04 -0700

    Updated configs.

--- app.py
+++ app.py
@@ -1,16 +1,49 @@
 from flask import Flask, render_template, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Integer
 from os import getenv
 from dotenv import load_dotenv
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
 
 app = Flask(__name__)
+
 # Set secret key
 app.config['SECRET_KEY'] = getenv("SECRET_KEY")
+# Set SQL Alchemy config
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DB_URI")
-
 db = SQLAlchemy(app)
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    # Profile pic
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    # One-to-Many relationship to Post
+    posts = db.relationship('Post', backref="author", lazy=True)
+
+    # Define how objects are printed out
+    def __repr__(self):
+
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    title = username = db.Column(db.String(120), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    
+    # Define how objects are printed out
+    def __repr__(self):
+
+        return f"User('{self.title}', '{self.date_posted}')"
 
 posts = [
     {
