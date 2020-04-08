a7cff75b7724940a77405384c94f66a00e8ecef3
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-04-08 14:03:54 -0700

    Added SQLAlchemy config.

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DB_URI")
 
+db = SQLAlchemy(app)
+
 posts = [
     {
         "author": "Dana",
