7afe58599cc58b2f339b37e78595c280d45629eb
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-04-15 17:20:52 -0700

    Added unit tests for routes.

---
+++ blog/tests/test_routes.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+from blog.routes import index
+from blog import app
+
+
+class RouteTests(TestCase):
+
+    def test_index_route(self):
+
+        with app.test_client() as tc:
+
+            response = tc.get('/')
+            redirect_response = tc.get('/home')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(redirect_response.status_code, 200)
+
+    # Test about page route
+    def test_about_route(self):
+
+        with app.test_client() as tc:
+
+            response = tc.get('/about')
+            self.assertEqual(response.status_code, 200)
+
+    # Test user registration route
+    def test_registration_route(self):
+
+        with app.test_client() as tc:
+
+            response = tc.get('/register')
+            self.assertEqual(response.status_code, 200)
+
+    # Test user login route
+    def test_login_route(self):
+
+        with app.test_client() as tc:
+
+            response = tc.get('/login')
+            self.assertEqual(response.status_code, 200)
